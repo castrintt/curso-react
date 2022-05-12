fe2bbe188e44c483dd62183ea51d7efe26c48eb4
castrintt <igordc38@gmail.com>
2022-05-12 00:47:16 -0300

    css global e em componentes

---
+++ aulas/react-e-css/css-global.js
@@ -0,0 +1,12 @@
+// Css global
+
+// o css global é utilizado para estilizar elementos em comum ou fazer um reset no CSS
+
+// utilizamos o arquivo index.css para isso
+
+// ele fica na pasta src (source)
+
+// geralmente o css global é importado dentro da pasta main.jsx
+
+// e la vamos estilizar estruturas globais --> nesse caso o body, ou alguma estrutura que tenha q ter as mesmas caracteristicas, como exemplo um titulo, que repete todos os estilos na pagina toda,
+//nesse caso o ideial é estilizar no arquivo index.css para que todos os titulos fiquem padronizados
