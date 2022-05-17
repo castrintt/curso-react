0e19bc026d9beec13754c5fa625c9ab8ad05ac85
castrintt <igordc38@gmail.com>
2022-05-17 10:55:21 -0300

    carregando dados da requsição POST e custom hooks

--- aulas/requisiçoes-https-react/carregando-dados-dinamicos.js
+++ aulas/requisiçoes-https-react/carregando-dados-dinamicos.js
@@ -1,7 +1,59 @@
+// Carregamento dinâmico de dados
+
+// se a requisição foi feita com sucesso, podemos adicionar o item a lista após o request
+
+// isso torna nossa aplicação mais performatica
+
+// utilizaremos o set do useState para isso
+
+//após feito a requisição POST com sucesso devemos adicionar ainda os dados resgatados no array de products usando o setProducts (onde temos acesso ao prev[no caso prevProducts])
+
+// dessa forma vamos adicionar ao array usando o spread operator tanto o prevProducts (que são os produtos presentes no state antes da mudança) e a nossa requisição
+
+// sintaxe da requisição
+
+import React, {useState, useEffect} from 'react'
+
+
+const [products, setProducts] = useState([])
+const [name, setName] = useState('')
+const [price, setPrice] = useState('')
+
+const product = {
+    name: name,
+    price: price
+}
+
+const url = 'http://localhost:3000/products'
+
+const res = await fetch(url, {
+    method:"POST",
+    headers: {
+        "Content-Type":"application/json"
+    },
+    body: JSON.stringify(product)
+})
+
+// pegando de forma assincrona os dados da nossa requisição
+const awaitRes = await res.json()
+
+//aqui temos acessos aos dados anteriores que por sua primeira instancia são os dados atuais 
+setProducts((prevProducts) => {
+    return [...prevProducts, awaitRes] // colocamos o awaitRes como item do array porque ele vai esperar a requisição do res ser feita(onde estamos fazendo nosso POST) e vai transformar em json() ai sim podemos enviar para o sistema
+})
+
+//resetando os inputs
+setName('')
+setPrice('')
+
+
+
+
+
+//dentro do componente ficaria assim:
+
 import { useEffect, useState } from 'react'
 
-//css
-import '../styles/App.css'
 
 function App() {
   //states
---
+++ aulas/requisiçoes-https-react/custom-hook-para-fetch.js
@@ -0,0 +1,11 @@
+// Custom hook para o fetch
+
+// É normal dividir funções que podem ser reaproveitadas em hooks
+
+// essa técnica é chamada de custom hook, e vamos criar um para o resgate de dados
+
+// os hooks geralmente ficam na pasta hooks
+
+//devemos utilizar o padrao useNomeDoHook
+
+// basicamente criamos essa função e exportamos ela para o componente! (como se fosse um arquivo css module)
